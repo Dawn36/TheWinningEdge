2d2797df305b933a412141399d6631887f51a520
Dawn36 <dawngill08@gmail.com>
2022-12-01 16:07:02 +0500

    done

--- resources/views/task/task_index.blade.php
+++ resources/views/task/task_index.blade.php
@@ -129,12 +129,12 @@
                                             <td>
                                                 <div class="badge badge-sm badge-light-info d-inline">{{ucwords($task[$i]->task_status)}}</div>
                                             </td>
-                                            <td>{{ucwords($task[$i]->first_name)}} {{ucwords($task[$i]->last_name)}} <br> {{ucwords($task[$i]->job)}}</td>
-                                            <td>{{$task[$i]->email}}
+                                            <td><a href="{{route('contact.show',$task[$i]->contact_id)}}" class="fw-bolder text-gray-800 text-hover-primary">{{ucwords($task[$i]->first_name)}} {{ucwords($task[$i]->last_name)}} </a> <br> <a href="{{route('contact.show',$task[$i]->contact_id)}}" class="fw-normal text-gray-800 text-hover-primary">{{ucwords($task[$i]->job)}}</a></td>
+                                            <td><a href="{{route('contact.show',$task[$i]->contact_id)}}" class="fw-bolder text-gray-800 text-hover-primary">{{$task[$i]->email}}</a>
                                                 <br>
-                                                <span class="fw-bolder">(d ) = </span>{{ucwords($task[$i]->phone_number)}}
+                                                <a href="{{route('contact.show',$task[$i]->contact_id)}}" class="fw-normal text-gray-800 text-hover-primary"><span class="fw-bolder">(d ) = </span>{{ucwords($task[$i]->phone_number)}}</a>
                                                 <br>
-                                                <span class="fw-bolder">(m) = </span>{{ucwords($task[$i]->mobile_phone)}}
+                                                <a href="{{route('contact.show',$task[$i]->contact_id)}}" class="fw-normal text-gray-800 text-hover-primary"><span class="fw-bolder">(m) = </span>{{ucwords($task[$i]->mobile_phone)}}</a>
                                             </td>
                                             @php $status=explode('_',$task[$i]->contact_status) @endphp
                                             <td>{{ ucwords($status[0]) }} {{ count($status) == "2" ? ucwords($status[1] ) : '' }}</td>
